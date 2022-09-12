c23cca7415a17d8cb293f81f4a4430aec4186076
Helena Oliveira <helena@informach.com.br>
2022-09-12 19:21:25 -0300

    finish app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
+
+import Image from "next/future/image"
+
+globalStyles()
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
-export default MyApp
+export default App
